81e306b0d4882279d21ed6833efe35fbd05f07d8
Lindsay Bordelon <lcbordelon@gmail.com>
2021-07-05 13:19:24 -0600

    added EEA function

--- RSA.py
+++ RSA.py
@@ -42,10 +42,25 @@ def FastModularExponentiation(b, k, m):
 
 
 
-b = 1568160359
-k = 20000446
-m = 645
+b = 2081
+k = 937
+m = 2537
 
 print(FastModularExponentiation(b, k, m))
 
 
+#EXTENDED EUCLIDEAN ALGORITHM
+def EEA(a, b):
+    x,y, u,v = 0,1, 1,0
+    while a != 0:
+        q, r = b//a, b%a
+        m, n = x-u*q, y-v*q
+        b,a, x,y, u,v = a,r, u,v, m,n
+    gcd = b
+    return gcd, x, y
+
+
+a = 13
+b = 2436
+
+print(EEA(a, b))
